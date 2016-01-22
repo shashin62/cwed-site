3a0c03e400d24fc2e550ab4b3e3ff059d29b6bc8
coolgeek <smartparsu@gmail.com>
2016-01-22 19:49:16 +0530

    First Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cpApp
+ * @description
+ * # cpApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cpApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
