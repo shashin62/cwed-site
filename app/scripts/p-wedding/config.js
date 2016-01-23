808ebdf03d4209d9e7d268e6fe10405ec1ac10ae
coolgeek <smartparsu@gmail.com>
2016-01-23 23:44:47 +0530

    Setup Grunt

---
+++ app/scripts/p-wedding/config.js
@@ -0,0 +1,7 @@
+"use strict";
+
+ angular.module('config', [])
+
+.constant('SETTINGS', {ApiVersion:'v1',ApiUrl:'http://cake3api.app/api/',FacebookAppId:721780927951719,GoogleClientId:'538803229923-to7uvp6jsdqfsrt9cd8340h3fm9sljqk.apps.googleusercontent.com',LinkedInClientId:'7523hg49rj3nik'})
+
+;
---
+++ app/scripts/p-wedding/index.js
@@ -0,0 +1,1 @@
+angular.module('p-wedding', ['ngResource', 'config', 'satellizer']);
